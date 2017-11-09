22f99eec9f2b85452a6911edb9407ce1c23d7262
till straumann <till.straumann@alumni.tu-berlin.de>
2017-11-08 19:20:05 -0800

    pass 'bsp name' to post-build.sh script; enabled uboot tools on arm targets

--- scripts/post-build.sh
+++ scripts/post-build.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
+# Gather and save version info
+
 # Check if we have all the info we need
 if [ $# -lt 1 -o ! -d $1 ] ; then
 	echo "Error: need target dir argument" >&2
 	exit 1
+fi
+if [ $# -lt 2 ] ; then
+	echo "Error: need BSP argument" >&2
+	exit 1
+else
+BSP_NAME="$2"
 fi
 if [ -z "${BR2_CONFIG}" ] ; then
 	echo "Error: need BR2_CONFIG envvar" >&2
@@ -69,3 +77,20 @@ if [ $? != 0 ] ; then
 	echo "Unable to obtain git description of 'site/'" >&2
 	exit 1
 fi
+
+# Amend inittab for nanopi
+case "${BSP_NAME}" in
+  *nanopi-neo)
+      if ! grep -q ttyGS0 $1/etc/inittab ; then
+         echo "Amending /etc/inittab for ttyGS0 (nanopi USB gadget)" >&2
+         echo 'ttyGS0::respawn:/sbin/getty -L ttyGS0 0 vt100 #USB_GADGET' >> $1/etc/inittab
+      fi
+      if [ ! -f $1/etc/fw_env.config ] ; then
+         echo "Creating /etc/fw_env.config for nanopi-neo" >&2
+         echo '# From u-boot.cfg (sunxi-h3-nanopi-neo); 10/2017'  > $1/etc/fw_env.config
+         echo '/dev/mmcblk0 0x88000 0x20000'                     >> $1/etc/fw_env.config
+      fi
+  ;;
+  *)
+  ;;
+esac
