b46ace1703bf1fa13b4460bcd778443a5fe3c9a6
Jeremy Lorelli <lorelli@slac.stanford.edu>
2024-02-16 17:52:36 -0800

    qemu: port forward 22 -> 8022 to test ssh from the host machine
    'ssh -p 8022 laci@0.0.0.0' can be used for access

--- scripts/run-qemu-i386.sh
+++ scripts/run-qemu-i386.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 cd "$(dirname "${BASH_SOURCE}")/../../output/images"
-qemu-system-i386 -m size=2048 -no-reboot -nographic -kernel ./bzImage -initrd ./rootfs.ext2 -append "console=ttyS0 init=/linuxrc root=/dev/ram0"
+qemu-system-i386 -m size=2048 -no-reboot -nographic -kernel ./bzImage -initrd ./rootfs.ext2 -append "console=ttyS0 init=/linuxrc root=/dev/ram0" \
+	-nic user,hostfwd=tcp::8022-:22
 
