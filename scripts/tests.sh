b7e682ee4217f893ec8a8945bc923a4ec6422384
Jeremy Lorelli <lorelli@slac.stanford.edu>
2024-11-08 19:10:32 -0800

    Change hard resource limits to unlimited on boot
    This required changes to busybox and buildroot:
    
      busybox: Moved the previous snippet that set the
       rlimits to main(). For some reason, when this
       code was in run(), the limits would not be
       properly handed down.
    
      dropbear: Patched dropbear to inherit RLIMIT_CORE
       from the parent rather than forcing the hard
       limit to 0. This prevents dropbear from
       overriding what we set in busybox at boot.
    
      tests: Updated test script to test the ulimit
       behavior.

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -20,7 +20,16 @@ function red {
     printf "\e[31m"
 }
 
-if ! ping -q -c 1 $1 > /dev/null; then
+function pass {
+    green; echo "PASS"; normal
+}
+
+function fail {
+    red; echo $@; normal
+    exit 1
+}
+
+if ! ping -q -c 1 $1 > $1.log; then
     echo "Host $1 may be down. Ping test failed"
     exit 1
 fi
@@ -29,43 +38,38 @@ green; echo "Host is up..."; normal
 
 # Check that telnetd isn't running at all
 printf "Checking that telnet doesn't exist..."
-if echo -e "\x1dclose\x0d" | telnet $1 > /dev/null 2>&1; then
-    red; echo "Telnet IS available! Test failed"; normal
-    exit 1
+if echo -e "\x1dclose\x0d" | telnet $1 > $1.log 2>&1; then
+    fail "Telnet IS available! Test failed"
 fi
-green; echo "PASS"; normal
+pass
 
 USERS="laci flaci acctf spear"
 
 # Check that the users exist in the first place
 printf "Checking for $USERS..."
 for user in $USERS; do
-    if ! ssh -o PasswordAuthentication=no $user@$1 "exit" > /dev/null 2>&1; then
-        red; echo "User $user does NOT exist passwordless!"; normal
-        exit 1
+    if ! ssh -o PasswordAuthentication=no $user@$1 "exit" > $1.log 2>&1; then
+        fail "User $user does NOT exist passwordless!"
     fi
 done
-green; echo "PASS"; normal
+pass
 
 # Check for matching ID/GID
 function check_uid_gid {
     printf "Checking that $2 has UID $3 and GID $4..."
-    _R=$(ssh -o PasswordAuthentication=no $2@$1 "echo USER=\$(id -u):\$(id -g)" 2> /dev/null)
+    _R=$(ssh -o PasswordAuthentication=no $2@$1 "echo USER=\$(id -u):\$(id -g)" 2> $1.log)
     P="USER=(.+):(.+)"
     if [[ $_R =~ $P ]]; then
         if [ "${BASH_REMATCH[1]}" != "$3" ]; then
-            red; echo "UID ${BASH_REMATCH[1]} does not match $3"; normal
-            exit 1
+            fail "UID ${BASH_REMATCH[1]} does not match $3"
         fi
         if [ "${BASH_REMATCH[2]}" != "$4" ]; then
-            red; echo "GID ${BASH_REMATCH[2]} does not match $4"; normal
-            exit 1
+            fail "GID ${BASH_REMATCH[2]} does not match $4"
         fi
     else
-        red; echo "No match found in $_R"; normal
-        exit 1
+        fail "No match found in $_R"
     fi
-    green; echo "PASS"; normal
+    pass
 }
 
 check_uid_gid $1 laci 8412 2211
@@ -76,20 +80,27 @@ check_uid_gid $1 spear 7753 1080
 # Check that chrt works passwordless
 printf "Checking for passwordless chrt..."
 for user in $USERS; do
-    if ! ssh -o PasswordAuthentication=no $user@$1 "chrt -r 4 sleep 1" > /dev/null 2>&1; then
-        red; echo "User $user cannot exec chrt passwordless!"; normal
-        exit 1
+    if ! ssh -o PasswordAuthentication=no $user@$1 "chrt -r 4 sleep 1" > $1.log 2>&1; then
+        fail "User $user cannot exec chrt passwordless!"
     fi
 done
-green; echo "PASS"; normal
+pass
 
 # Check that sudo works passwordless
 printf "Checking for passwordless sudo with chrt..."
 for user in $USERS; do
-    if ! ssh -o PasswordAuthentication=no $user@$1 "sudo chrt -r 4 sleep 1" > /dev/null 2>&1; then
-        red; echo "User $user cannot exec sudo passwordless!"; normal
-        exit 1
+    if ! ssh -o PasswordAuthentication=no $user@$1 "sudo chrt -r 4 sleep 1" > $1.log 2>&1; then
+        fail "User $user cannot exec sudo passwordless!"
     fi
 done
-green; echo "PASS"; normal
+pass
+
+# Validate ulimits are configured correctly
+printf "Checking that soft limits may be adjusted using ulimit..."
+for lim in c r l; do
+    if ! ssh -o PasswordAuthentication=no laci@$1 "ulimit -S$lim unlimited" > $1.log 2>&1; then
+        fail "Unable to set soft limit to unlimited with 'ulimit -S$lim unlimited'. Hard limits are probably wrong"
+    fi
+done
+pass
 
