ad35842c36d73dd5c9c7efdb73c5041ac8b2efd3
Marcio Donadio <marcio@slac.stanford.edu>
2025-04-30 12:25:32 -0700

    With Buildroot 2025 we don't need any external package but I didn't want to lose the reference on how to use the infrastructure for external packages. So, I converted libeudev into a template for future reference.

--- br2-external/external.mk
+++ br2-external/external.mk
@@ -1,1 +1,2 @@
-include $(sort $(wildcard $(BR2_EXTERNAL_SITE_SLAC_PATH)/packages/*/*.mk))
+# Uncomment this line when there's an external package to be installed
+#include $(sort $(wildcard $(BR2_EXTERNAL_SITE_SLAC_PATH)/packages/*/*.mk))
